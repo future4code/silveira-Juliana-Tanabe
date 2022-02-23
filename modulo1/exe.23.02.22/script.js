8d9979a7ad27571ee15745afffa31c412486ed29
JuliMaiTan <jumtpro@gmail.com>
2022-02-23 16:40:19 -0300

    nova atividade

---
+++ modulo1/exe.23.02.22/script.js
@@ -0,0 +1,20 @@
+1. O código imprime no console a contagem de 1 a 5
+
+2.
+a. Serão impressos todos os números abaixo de 18
+
+b.  
+const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {
+  if (numero.length < 10) {
+		console.log(numero)
+	}
+}
+
+3.
+*
+**
+***
+****
+
+
